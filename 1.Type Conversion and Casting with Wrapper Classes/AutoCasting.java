2039fa340775491a598ffc05225cdb4e4a605777
Bhargavi1439 <chbhargavi.06@gmail.com>
2025-08-02 09:20:34 +0530

    Add files via upload

---
+++ 1.Type Conversion and Casting with Wrapper Classes/AutoCasting.java
@@ -0,0 +1,10 @@
+package MyProject_Day1;
+
+public class AutoCasting {
+	public static void main (String[] args) {
+		int a = 10;
+		double d = a;
+		System.out.println("Integer: " + a);
+		System.out.println("Double : "+d);	
+	}
+}
---
+++ 1.Type Conversion and Casting with Wrapper Classes/ManualCasting.java
@@ -0,0 +1,10 @@
+package MyProject_Day1;
+
+public class ManualCasting {
+	public static void main(String[] args) {
+		double d = 9.78;
+		int a = (int)d;
+		System.out.println("Double value: " + d);
+		System.out.println("Integer value: "+a);
+	}
+}
